88a2566f16e5ff4113e48f7565641e08dd0e4f80
Premchand Thurvas <Premchand.Thurvas@microfocus.com>
2022-09-12 21:19:33 +0530

    init

---
+++ src/fix-the-errors.ts
@@ -0,0 +1,17 @@
+const num1: number = '3';
+num1 = num1 + 7; // 10
+console.log(num1);
+
+// Do not shuffle the code lines
+function doSomething (val: boolean) {
+    if (val) {
+        const str = 'done';
+    }
+
+    console.log(str);
+}
+
+// Do not change the type of the assignment value
+let employeeId: string = 'MF1001';
+employeeId = 1001;
+console.log(employeeId);
