491ea77f9d2e9e7f6d8caf3290246a2ab1a75813
Premchand Thurvas <Premchand.Thurvas@microfocus.com>
2022-09-13 13:37:26 +0530

    update

--- src/exercise3.ts
+++ src/exercise3.ts
@@ -1,22 +1,44 @@
 const employeeNames = ["Jack", "Mike", "Dave"];
 // 1. Use for...of to print the elements of the array.
+for (const s of employeeNames) {
+    console.log(s);
+};
 
 // 2. Create a function with REST parameter and print the parameter values.
+function restfunction(a: number, ...b: any) {
+    console.log('a: ' + a + 'b:' + b);
+    console.log(a + b);
+};
+
+restfunction(1, 2, 3);
 
 // 3. Create a new array using spread operator
-// const array1 = [1, 2, 3];
-// const array2 = [4, 5, 6, 7];
-// const newArray
-// console.log(newArray);
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6, 7];
+const newArray = [...array1, ...array2];
+console.log(newArray);
+console.log(newArray[4]);
 
 // 4. Unpack the employeeNames array values and store in variables.
+const [x, y, z] = employeeNames;
+console.log(x + y + z);
 
 // 5. Join the employeeArray value strings using template literals.
 // Do not use + to join strings as shown below.
 // console.log('Employees: ' + employeeNames[0] + ", " + employeeNames[1] + ", " + employeeNames[2]);
+console.log(`Employees: ${employeeNames[0].toUpperCase()}, ${employeeNames[1] === '1'}, ${employeeNames[2]}`);
 
-// 6. Create a array function that returns the emmployee name by taking the index as input.
-// Uncommnet and complete the code
-// ... Add function here ...
-// const empName = getEmployee(1);
-// console.log(empName);
+// 6. Create a arrow function that returns the emmployee name by taking the index as input.
+// Uncomment and complete the code
+const getEmployee = (index: number): string => {
+    const emp = 'Hello' +employeeNames[index];
+    return emp;
+};
+const empName = getEmployee(1);
+console.log(empName);
+
+// Custom Types
+type colorId = 'red' | 'green' | 'blue';
+const redColour: colorId = 'red';
+const yellowColour: colorId = 'yellow';
+if (redColour === 'RED') { } // error
--- src/fix-the-errors.ts
+++ src/fix-the-errors.ts
@@ -1,9 +1,9 @@
-const num1: number = '3';
+let num1: number = 3;
 num1 = num1 + 7; // 10
 console.log(num1);
 
 // Do not change the type of the assignment value
-let employeeId: string = 'MF1001';
+let employeeId: string | number = 'MF1001';
 employeeId = 1001;
 console.log(employeeId);
 
@@ -12,11 +12,33 @@ let employee: {
     id: number 
 };
 employee = {
-    name: 123,
-    id: xyz
+    name: 'John',
+    id: 123
 };
 
 // Create a employee object type array and push employee data to the array
-// let employeeList = .................
+type employeeType = {
+    name: string,
+    id: number 
+};
+
+let employeeList: [employeeType] = [{
+    name: 'Some name',
+    id: 1
+}];
+
+
+let zemployeeList: {
+    name: string,
+    id: number 
+}[] = [];
+
+
+
+employeeList.push({
+    name: 'Prem',
+    id: 101
+})
 
 // Print the type of the employeeList object
+console.log(typeof(employeeList));
